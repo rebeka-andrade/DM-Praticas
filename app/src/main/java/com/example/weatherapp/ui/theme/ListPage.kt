1c9ca99cf789ed955400b22b250b659d77dd6021
Peixe-blue <ras52@discente.ifpe.edu.br>
2025-10-30 08:54:39 -0300

    Prática 3 – Parte 1

--- app/src/main/java/com/example/weatherapp/ui/theme/ListPage.kt
+++ app/src/main/java/com/example/weatherapp/ui/theme/ListPage.kt
@@ -1,23 +1,37 @@
 package com.example.weatherapp.ui.theme
 
 import android.app.Activity
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.widget.Toast
 import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
@@ -27,22 +41,70 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.weatherapp.MainActivity
+import com.example.weatherapp.model.City
 
 @Preview(showBackground = true)
 @Composable
 fun ListPage(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize()
-            .background(Color.Magenta)
-            .wrapContentSize(Alignment.Center)
+    val cityList = remember { getCities().toMutableStateList() }
+    val activity = LocalActivity.current as Activity // Para os Toasts
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
     ) {
-        Text(
-            text = "Home",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = modifier.align(CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+        items(cityList, key = { it.name }) { city ->
+            CityItem(city = city, onClose = { Toast.makeText(activity, "Excluindo Cidade!", Toast.LENGTH_LONG).show()
+                activity.startActivity(
+                    Intent(activity, MainActivity::class.java).setFlags(
+                        FLAG_ACTIVITY_SINGLE_TOP
+                    )
+                )
+            }, onClick = {
+                Toast.makeText(activity, "Selecionando Cidade!", Toast.LENGTH_LONG).show()
+                activity.startActivity(
+                    Intent(activity, MainActivity::class.java).setFlags(
+                        FLAG_ACTIVITY_SINGLE_TOP
+                    )
+                )
+            })
+        }
+    }
+}
+
+private fun getCities() = List(20) { i ->
+    City(name = "Cidade $i", weather = "Carregando clima...")
+}
+
+@Composable
+fun CityItem(
+    city: City,
+    onClick: () -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth().padding(8.dp).clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            Icons.Rounded.FavoriteBorder,
+            contentDescription = ""
         )
+        Spacer(modifier = Modifier.size(12.dp))
+        Column(modifier = modifier.weight(1f)) {
+            Text(modifier = Modifier,
+                text = city.name,
+                fontSize = 24.sp)
+            Text(modifier = Modifier,
+                text = city.weather?:"Carregando clima...",
+
+                fontSize = 16.sp)
+
+        }
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
     }
 }
