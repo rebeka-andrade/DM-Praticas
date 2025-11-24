0881bfa93387c588249376f293e8fbdcee6f77cf
Peixe-blue <rebekaandrade006@gmail.com>
2025-11-24 08:58:11 -0300

    Prática 5 – Parte 3 - com modificação!

--- app/src/main/java/com/example/weatherapp/RegisterActivity.kt
+++ app/src/main/java/com/example/weatherapp/RegisterActivity.kt
@@ -12,6 +12,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+import android.app.Activity
+import android.widget.Toast
+import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class RegisterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +43,93 @@ class RegisterActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     RegisterPage(modifier = Modifier.padding(innerPadding))
                 }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterPage(modifier: Modifier = Modifier) {
+    var nome by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordConfir by rememberSaveable { mutableStateOf("") }
+    val activity = LocalActivity.current as Activity
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = CenterHorizontally
+    ) {
+        Text(
+            text = "Bem-vindo/a!",
+            fontSize = 24.sp
+        )
+        Spacer(modifier = modifier.size(24.dp))
+        OutlinedTextField(
+            value = nome,
+            label = { Text(text = "Digite seu nome") },
+            modifier = Modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { nome = it }
+        )
+        OutlinedTextField(
+            value = email,
+            label = { Text(text = "Digite seu e-mail") },
+            modifier = Modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { email = it }
+        )
+        OutlinedTextField(
+            value = password,
+            label = { Text(text = "Digite sua senha") },
+            modifier = Modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        OutlinedTextField(
+            value = passwordConfir,
+            label = { Text(text = "Confirme sua senha") },
+            modifier = Modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { passwordConfir = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = modifier.size(24.dp))
+        Row(modifier = modifier) {
+            Button(
+                onClick = {
+                    Firebase.auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(activity) { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(
+                                    activity,
+                                    "Registro OK!", Toast.LENGTH_LONG
+                                ).show()
+                            } else {
+                                Toast.makeText(
+                                    activity,
+                                    "Registro FALHOU!", Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                },
+                enabled = (nome.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && passwordConfir.isNotEmpty()) && password == passwordConfir
+            ) {
+                Text("Registrar")
+            }
+            Spacer(modifier = modifier.size(24.dp))
+            Button(
+                onClick = { nome = ""; email = ""; password = ""; passwordConfir = ""; }
+            ) {
+                Text("Limpar")
+            }
+            Spacer(modifier = modifier.size(24.dp))
+            Button(
+                onClick = {
+                    activity.finish()
+                }
+            ) {
+                Text("Sair")
             }
         }
     }
